697906623e9c4f810879fdb8415ddd2aad2696dd
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-30 22:28:10 -0300

    add page counter example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 const Home = () => {
-    return <h1>Hello world</h1>;
+    const [value, setValue] = useState(0);
+
+    return (
+        <>
+            <h1>Incrementando: {value}</h1>
+            <button onClick={() => setValue(value + 1)}>Incrementar</button>
+        </>
+    );
 };
 export default Home;
