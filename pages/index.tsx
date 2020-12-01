c68a03487f22a80dba4cdfa55ad01f2a6461bd8d
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-30 23:25:28 -0300

    add modules alias

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+// import Head from 'next/head';
+import Head from '@components/Head';
+const Home = () => {
+    return (
+        <>
+            <Head title="Homepage" />
+            <h1>Hello world</h1>
+        </>
+    );
+};
+export default Home;
