d37cf989e4315c4cb38e984ba7a319b6ff11c2f1
carlos <carloskauanmoreiradesousa@gmail.com>
2020-12-01 22:12:04 -0300

    update app to better performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,62 @@
 import Head from '@components/Head';
 import { Container, PostsContainer } from '../styles/Home';
 import Presentation from '@components/Sidebar';
-const Home = () => {
+import axios from 'axios';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+interface Post {
+    id: string;
+    title: string;
+    slug: string;
+    description: string;
+    content: string;
+    created_at: string;
+}
+interface Props {
+    posts: Post[];
+}
+const Home = ({ posts }: Props) => {
     return (
         <Container>
             <Head title="Homepage" />
             <Presentation />
             <PostsContainer>
-                <a href="posts/abc">
-                    <img
-                        src="https://github.com/ckauan.png"
-                        alt="Carlos Kauãn"
-                    />
-                    <div>
-                        <small>31 de Outubro de 2020 • 2 min de leitura</small>
-                        <h2>
-                            Mostrando últimos posts e vídeos no Github Profile
-                        </h2>
-                        <p>
-                            Como utilizar o Github Actions para manter seu
-                            profile atualizado e bonito.
-                        </p>
-                    </div>
-                </a>
-                <a href="">
-                    <img
-                        src="https://github.com/ckauan.png"
-                        alt="Carlos Kauãn"
-                    />
-                    <div>
-                        <small>31 de Outubro de 2020 • 2 min de leitura</small>
-                        <h2>
-                            Mostrando últimos posts e vídeos no Github Profile
-                        </h2>
-                        <p>
-                            Como utilizar o Github Actions para manter seu
-                            profile atualizado e bonito.
-                        </p>
-                    </div>
-                </a>
-                <a href="">
-                    <img
-                        src="https://github.com/ckauan.png"
-                        alt="Carlos Kauãn"
-                    />
-                    <div>
-                        <small>31 de Outubro de 2020 • 2 min de leitura</small>
-                        <h2>
-                            Mostrando últimos posts e vídeos no Github Profile
-                        </h2>
-                        <p>
-                            Como utilizar o Github Actions para manter seu
-                            profile atualizado e bonito.
-                        </p>
-                    </div>
-                </a>
-                <a href="">
-                    <img
-                        src="https://github.com/ckauan.png"
-                        alt="Carlos Kauãn"
-                    />
-                    <div>
-                        <small>31 de Outubro de 2020 • 2 min de leitura</small>
-                        <h2>
-                            Mostrando últimos posts e vídeos no Github Profile
-                        </h2>
-                        <p>
-                            Como utilizar o Github Actions para manter seu
-                            profile atualizado e bonito.
-                        </p>
-                    </div>
-                </a>
+                {posts &&
+                    posts.map((post) => (
+                        <Link
+                            href={`posts/${post.slug}`}
+                            key={post.id}
+                            passHref
+                        >
+                            <a>
+                                <img
+                                    src="https://github.com/ckauan.png"
+                                    alt="Carlos Kauãn"
+                                />
+                                <div>
+                                    <small>
+                                        {new Date(
+                                            post.created_at
+                                        ).toLocaleDateString()}
+                                    </small>
+                                    <h2>{post.title}</h2>
+                                    <p>{post.description}</p>
+                                </div>
+                            </a>
+                        </Link>
+                    ))}
             </PostsContainer>
         </Container>
     );
 };
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await axios.get('http://localhost:5500/posts');
+    return {
+        props: {
+            posts: data,
+        },
+        redirect: 3600,
+    };
+};
