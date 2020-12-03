def632577d859666a727c921567861be158c0405
carlos <carloskauanmoreiradesousa@gmail.com>
2020-12-02 21:48:50 -0300

    add loading component

---
+++ src/styles/Theme.ts
@@ -0,0 +1,15 @@
+export default {
+    primary: '#648dae',
+    secondary: '#aa647b',
+    error: '#d32f2f',
+    warning: '#f57c00',
+    info: '#1976d2',
+    success: '#388e3c',
+    dark: {
+        primary: '#141414',
+        secondary: '#202024',
+        tertiary: '#2C3032',
+        quaternary: '#636363',
+        text: '#A2A2A3',
+    },
+};
